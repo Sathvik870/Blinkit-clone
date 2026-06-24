86b2568e95ee6619096c6e5b4533be176c56a3cf
root <root@Sathvik.taila67c7b.ts.net>
2026-06-24 14:19:06 +0530

    For docker

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== "test") {
 }
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const HOST = "0.0.0.0";
 
 function findLocalIp() {
